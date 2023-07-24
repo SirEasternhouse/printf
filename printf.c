f7d05bd789fe02a8996c3a5edc5dfb976339683b
SirEasternhouse <aso21arnold.oosthuizen@gmail.com>
2023-07-24 15:58:05 -0700

    addition of initial files

---
+++ printf.c
@@ -0,0 +1,18 @@
+/* possible libraries we need*/
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/*Betty-style*/
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int _printf(const char *format, ...) /*start of the function*/
+{
+}
